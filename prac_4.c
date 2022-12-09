e1d2ab17b4eb2aea25eec396e1ec36ac17b9fb69
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2022-12-09 11:45:49 +0530

    Print We Are Proud To Be Indians

---
+++ prac_4.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+
+void *test();
+
+int main()
+{
+	pthread_t tid;
+	pthread_attr_t attr;
+	pthread_attr_init( &attr );
+	
+	pthread_create( &tid, &attr, test, NULL);
+	pthread_join(tid, NULL);
+	
+	printf("\n Exiting main thread. \n");
+	return 0;
+}
+
+
+void *test()
+{
+	
+	printf("We are proud to be Indians. ");
+	pthread_exit(NULL);
+}
+
